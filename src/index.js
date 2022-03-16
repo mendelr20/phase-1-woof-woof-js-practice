8a6d3ba93fb19d78ca795a062f0ac75354717b63
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-03-16 18:05:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,14 @@ const dogBar = document.getElementById('dog-bar')
 const filterBtn = document.getElementById('good-dog-filter')
 const info = document.getElementById('dog-info')
 const newSpan = document.createElement("span");
-dogBar.append(newSpan)
+// dogBar.append(newSpan)
 fetch(Url)
 .then(res => res.json)
 then((dogs)=> {
     dogs.forEach((dog) => {
         monsterContainer.append(renderMonster(monster), document.createElement('hr'))  
         dogBar.append(newSpan)
-        monsterSpan.innerHTML = `
-            <h1>${dog.name}</h1>
-            `
+        monsterSpan.innerHTML = dog.name
     })
 })
 })
