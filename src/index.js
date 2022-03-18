40208da4e36a37f63a172c0e21ee79290a171e75
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-03-18 11:03:38 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const Url = 'http://localhost:3000/pups'
 const dogBar = document.getElementById('dog-bar')
 const filterBtn = document.getElementById('good-dog-filter')
 const info = document.getElementById('dog-info')
-//dogBar.append(newSpan)
 fetch(Url)
 .then(res => res.json())
 .then((dogs)=> {
@@ -11,22 +10,62 @@ fetch(Url)
         const newSpan = document.createElement("span");
         dogBar.append(newSpan)
         newSpan.innerHTML = dog.name
-        newSpan.addEventListener('click', () => {
-        if (newSpan.innerHTML === false) {
-            const img = document.createElement("img");
-            const h2 = document.createElement("h2");
-            const btn = document.createElement("button")
-            img.src = dog.image
-            h2.innerHTML = dog.name
-            btn.innerHTML = dog.isGoodDog
-            info.append(img,h2)
-          } else {
-            img.src = dog.image
-            h2.innerHTML = dog.name
-            btn.innerHTML = dog.isGoodDog
-            info.append(img,h2)
-            }              
-        
+        newSpan.addEventListener('click', () => {  
+        info.innerHTML = ''
+        const img = document.createElement("img");
+        const h2 = document.createElement("h2");
+        const btn = document.createElement("button")
+        img.src = dog.image
+        h2.innerHTML = dog.name
+        btn.id = 'goodBad'
+        btn.innerHTML = dog.isGoodDog
+        info.append(img,h2, btn)
+        id = dog.id
+        btn.addEventListener('click', () => {
+            if(dog.isGoodDog === true){
+                fetch(`http://localhost:3000/pups/${id}`, {
+                    method: "PATCH",
+                    headers: {"Accept": "application/json",
+                        "Content-type": "application/json"},
+                    body: JSON.stringify({
+                        isGoodDog: false
+                    })
+                })
+                .then(res => res.json())
+                .then((dog) => {
+                    info.innerHTML = ''
+                    const img = document.createElement("img");
+                    const h2 = document.createElement("h2");
+                    const btn = document.createElement("button")
+                    img.src = dog.image
+                    h2.innerHTML = dog.name
+                    btn.id = 'goodBad'
+                    btn.innerHTML = dog.isGoodDog
+                    info.append(img,h2, btn)
+                })
+            } else {
+                fetch(`http://localhost:3000/pups/${id}`, {
+                    method: "PATCH",
+                    headers: {"Accept": "application/json",
+                        "Content-type": "application/json"},
+                    body: JSON.stringify({
+                        isGoodDog: true
+                    })
+                })
+                .then(res => res.json())
+                .then((dog) => {
+                    info.innerHTML = ''
+                    const img = document.createElement("img");
+                    const h2 = document.createElement("h2");
+                    const btn = document.createElement("button")
+                    img.src = dog.image
+                    h2.innerHTML = dog.name
+                    btn.id = 'goodBad'
+                    btn.innerHTML = dog.isGoodDog
+                    info.append(img,h2, btn)
+                })
+            }
+        })
         })
     })
     
